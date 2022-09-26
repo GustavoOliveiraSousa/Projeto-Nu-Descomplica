d118c2bce8decfd0c194bd69f2d30980948ab84b
Gustavo <gs98134@gmail.com>
2022-09-26 13:30:38 -0300

    Condicionais

--- React/React-app/app/src/App.js
+++ React/React-app/app/src/App.js
@@ -14,12 +14,43 @@ function Botao(props) {
   )
 }
 
+function PessoaDeMaior(props) {
+  return (
+    <strong>Maior de idade</strong>
+  )
+}
+
+function PessoaDeMenor(props) {
+  return (
+    <strong>Menor de idade</strong>
+  )
+}
+
+function Pessoa(props) {
+
+  if (props.idade < 10) {
+    return (
+      <h4>ÁREA RESTRITA</h4>
+    )
+  } else {
+    return (
+      <section>
+        Você tem {`${props.idade} anos, portanto é: `}
+        {props.idade >= 18 ? <PessoaDeMaior /> : <PessoaDeMenor />}
+      </section>
+    )
+  }
+}
+
 function App() {
 
   const labelBotao = 'Entre aqui!'
   return (
     <div className="App">
       <Titulo />
+      <Pessoa idade={19} />
+      <Pessoa idade={14} />
+      <Pessoa idade={9} />
       <article>
         <h2>Subtitulo</h2>
         <p>abcd abcd abcd abcd abcd </p>
