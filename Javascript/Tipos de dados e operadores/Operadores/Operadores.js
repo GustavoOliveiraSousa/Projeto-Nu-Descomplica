cbb5d94c0d02c299b1d1ba24a0b6cd12d5c0c217
Gustavo <gs98134@gmail.com>
2022-10-26 20:03:50 -0300

    Tipos de dados e operadoes

---
+++ Javascript/Tipos de dados e operadores/Operadores/Operadores.js
@@ -0,0 +1,29 @@
+var n = 3;
+
+// n = n - 10;
+n *= 10
+console.log(n);
+
+var x = 10;
+x++;
+console.log(x);
+
+a = 30;
+b = 20;
+c = 40;
+console.log(n > x);
+console.log(n <= x && b != c);
+
+// Verificar se a é maior que b e b é divisível por 2
+console.log(a > b && b % 2 == 0);
+
+// Verificar se b é maior ou igual a a ou b é divisível por 2 e o resultado é 2
+console.log(a >= b || b % 2 == 2);
+
+// Verificar se a é maior que b e b é maior ou igual a 5
+console.log(a > b && b >= 5);
+
+// Verificar se b é par ou impar usando ternário
+console.log(b % 2 == 0 ? "Par" : "Impar");
+
+
