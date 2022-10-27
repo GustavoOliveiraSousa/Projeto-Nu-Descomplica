06c65e45ab2766dafa9898245eb9ddf223a3450c
Gustavo <gs98134@gmail.com>
2022-10-26 21:39:03 -0300

    Arrays básicos

---
+++ Javascript/Arrays/Arrays.js
@@ -0,0 +1,20 @@
+var valores = [8, 3, 5, 9, 1, 4, 6, 2];
+console.log(valores[4]);
+
+for (let i = 0; i < valores.length; i++) {
+    console.log("Posição: " + i + "     Valor: " + valores[i]);;
+}
+
+var carros = []
+carros[0] = "Volvo";
+carros[1] = "Corolla";
+
+var motos = new Array("BMW", "Yamaha", "Honda");
+
+//Calcular média
+var aux = 0;
+for (let i = 0; i < valores.length; i++) {
+    aux += valores[i];
+}
+var media = aux / valores.length;
+console.log(media);
