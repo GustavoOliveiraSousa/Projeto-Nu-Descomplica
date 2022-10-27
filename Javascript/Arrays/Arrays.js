bb0716a8146e67830cbb226aba42c81de346ca41
Gustavo <gs98134@gmail.com>
2022-10-26 22:20:09 -0300

    Métodos de arrays

--- Javascript/Arrays/Arrays.js
+++ Javascript/Arrays/Arrays.js
@@ -20,3 +20,32 @@ var media = aux / valores.length;
 console.log(media);
 
 //---------------------------------------------------------------------------------------------
+
+console.log("Array inverso: " + valores.reverse());
+
+var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(arr1.join("|"));
+
+var retirado = arr1.shift();
+console.log(retirado);
+console.log(arr1);
+
+var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log("Posição do número 6: " + arr1.indexOf(6));
+
+arr1.push(100);
+console.log(arr1);
+
+arr1.pop();
+console.log(arr1);
+
+//crie um array só com números positivos a partir do array [-2,5,-4,-8,9,3,6,-9,3,5,-3]
+var arrayoriginal = [-2, 5, -4, -8, 9, 3, 6, -9, 3, 5, -3, 8, 12, 4]
+var arrayPar = [];
+
+for (let index = 0; index < arrayoriginal.length; index++) {
+    if (arrayoriginal[index] % 2 == 0 && arrayoriginal[index] >= 0) {
+        arrayPar.push(arrayoriginal[index]);
+    }
+}
+console.log(arrayPar);
