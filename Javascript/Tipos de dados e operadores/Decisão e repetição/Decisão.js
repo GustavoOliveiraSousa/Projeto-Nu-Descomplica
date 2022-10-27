8e6b4955f0257f71b7f4e6673076fb66988fb4b6
Gustavo <gs98134@gmail.com>
2022-10-26 21:09:42 -0300

    While e DoWhile

--- Javascript/Tipos de dados e operadores/Decisão e repetição/Decisão.js
+++ Javascript/Tipos de dados e operadores/Decisão e repetição/Decisão.js
@@ -53,3 +53,14 @@ for (let c of carro) {
     console.log(c.marca);
 }
 //---------------------------------------------------------------------------------------------
+var c = 1;
+while (c <= 10) {
+    console.log(c);
+    c++;
+}
+
+var t = 1
+do {
+    console.log(t);
+    t++;
+} while (t <= 15);
