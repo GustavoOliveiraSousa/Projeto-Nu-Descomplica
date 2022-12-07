577b22378dc0971e5c4a088ed0689981ab5ff9eb
Gustavo Sousa <gs98134@gmail.com>
2022-12-06 21:51:35 -0300

    Formulários Não controlados

--- React/React-app/app/src/Formulario.js
+++ React/React-app/app/src/Formulario.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 
 export const Formulario = () => {
     const [fieldValue, setFieldValue] = useState({
@@ -7,6 +7,9 @@ export const Formulario = () => {
         CPF: '',
         RG: '',
     })
+
+    const fieldCidade = useRef(null)
+
     const handleChange = (e) => {
         console.log(e.target.name, e.target.value);
         setFieldValue({
@@ -14,13 +17,20 @@ export const Formulario = () => {
             [e.target.name]: e.target.value
         })
     }
+
+    const handleSubmite = (e) => {
+        e.preventDefault()
+
+        console.log(fieldCidade.current.value);
+    }
     return (
         <div>
             <form>
                 <input type="text" placeholder="nome" name="nome" value={fieldValue.nome} onChange={handleChange} />
-                <input type="text" placeholder="cidade" name="cidade" value={fieldValue.cidade} onChange={handleChange} />
+                <input type="text" placeholder="cidade" name="cidade" ref={fieldCidade} onChange={handleChange} />
                 <input type="text" placeholder="CPF" name="CPF" value={fieldValue.CPF} onChange={handleChange} />
                 <input type="text" placeholder="RG" name="RG" value={fieldValue.RG} onChange={handleChange} />
+                <button onSubmit={handleSubmite} type="submite">Enviar</button>
             </form>
         </div>
 
