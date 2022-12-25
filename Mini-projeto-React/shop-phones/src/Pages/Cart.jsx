60d5a48fcb0fd0a675afb990841bd4b962cea1e0
Gustavo Sousa <gs98134@gmail.com>
2022-12-25 11:45:36 -0300

    Formatando os preços com API Intl.NumberFormat

--- Mini-projeto-React/shop-phones/src/Pages/Cart.jsx
+++ Mini-projeto-React/shop-phones/src/Pages/Cart.jsx
@@ -3,6 +3,7 @@ import { getItem, setItem } from "../services/LocalStoregeFuncs";
 import { BsFillCartDashFill } from 'react-icons/bs'
 import '../styles/Style.css'
 import { Headercart } from "../Components/Header";
+import UINumber from "../UNNumber";
 
 export const Cart = () => {
     const [data, setData] = useState(getItem('carrinhoNu') || [])
@@ -19,13 +20,14 @@ export const Cart = () => {
         <div className="Body">
             <Headercart />
             <h1 className="C-NomeCarrinho">Carrinho</h1>
-            <h3 className="C-Subtotal" id="subtotal">{`Total R$ ${subTotal}`}</h3>
+            <UINumber className="C-Subtotal" id="subtotal">{subTotal}</UINumber>
+
             <div className="C-Produto">
                 {
                     data.map((e) => (
                         <div className="C-ProdutoBody" key={e.id}>
                             <h4 className="C-Titulo">{e.title}</h4>
-                            <h4 className="C-Preco">{`R$ ${e.price}`}</h4>
+                            <UINumber className="C-Preco">{e.price}</UINumber>
                             <img className="C-Imagem" alt="." src={e.thumbnail} />
                             <button className="C-Botao" onClick={() => removeItem(e)}>
                                 <BsFillCartDashFill className="C-BotaoInterno" />
